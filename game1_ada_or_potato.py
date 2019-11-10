a04597a57e1a8bfd962100af09203cb1b9d2c79b
laurenbaron <lbaron@udel.edu>
2019-11-09 19:56:18 -0500

    finished ada or potato

--- game1_ada_or_potato.py
+++ game1_ada_or_potato.py
@@ -2,17 +2,18 @@ import arcade
 import random
 
 
+
 # Define constants
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = arcade.color.BLACK
 GAME_TITLE = "Ada or Potato"
 GAME_SPEED = 1/60
 TIMER_MAXIMUM = 100
 
 #Preload images
-IMAGE_ADA=arcade.load_texture("images/ada.png")
-IMAGE_POTATO=arcade.load_texture("images/potato.png")
+IMAGE_ADA=arcade.load_texture("images/ada.png", scale=.75)
+IMAGE_POTATO=arcade.load_texture("images/potato.png", scale=.2)
 
 
 class AlternatingImages(arcade.Sprite):
@@ -21,7 +22,7 @@ class AlternatingImages(arcade.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.phase = 'waiting'
+        self.phase = 'ada'
         self.timer = 0
         self.center_x = WINDOW_WIDTH / 2
         self.center_y = WINDOW_HEIGHT / 2
@@ -44,8 +45,10 @@ class AlternatingImages(arcade.Sprite):
     def switch_image(self):
         if self.texture==IMAGE_ADA:
             self.texture=IMAGE_POTATO
+            self.phase='potato'
         else:
             self.texture=IMAGE_ADA
+            self.phase='ada'
 
 
 class AdaOrPotato(arcade.Window):
@@ -55,7 +58,7 @@ class AdaOrPotato(arcade.Window):
         """ Initialize variables """
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         self.logo_list = None
-        points=0
+        self.points=0
 
     def setup(self):
         """ Setup the game (or reset the game) """
@@ -67,19 +70,18 @@ class AdaOrPotato(arcade.Window):
         """ Called when it is time to draw the world """
         arcade.start_render()
         self.logo_list.draw()
-        arcade.draw_text("points: " + str(self.points), 100, 100, arcade.csscolor.WHITE)
+        output = f"Score: {self.points}"
+        arcade.draw_text(output, 10, 50, arcade.color.WHITE, 13)
 
     def on_update(self, delta_time):
         """ Called every frame of the game (1/GAME_SPEED times per second)"""
         self.logo_list.update()
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-        if self.logo_list[0].texture=="IMAGE_ADA":
-            self.points+=1
+        if self.logo_list[0].phase == "ada":
+            self.points += 1
         else:
-            self.points-=1
-        print(self.points)
-
+            self.points -= 1
 
 def main():
     window = AdaOrPotato()
